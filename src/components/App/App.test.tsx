eafd786e9f13900c5f3c6d7e566026f6a3a0ecdc
Ivet Lopez <ivet19@live.com>
2025-04-08 21:51:18 +0200

    Create App tests

---
+++ src/components/App/App.test.tsx
@@ -0,0 +1,38 @@
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import App from "./App";
+
+describe("Given the App component", () => {
+  describe("When it renders", () => {
+    test("Then it should show 'Monuments' inside a level 1 heading", () => {
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+
+      const appTitle = screen.queryByRole("heading", {
+        name: /monuments/i,
+        level: 1,
+      });
+
+      screen.debug();
+
+      expect(appTitle).toBeVisible();
+    });
+    test("Then it should show a 'Monuments' and an 'Add new monument' link", () => {
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+
+      const monumentsLink = screen.queryByRole("link", { name: /monuments/i });
+
+      expect(monumentsLink).toBeVisible();
+
+      expect(monumentsLink).toBeVisible();
+    });
+  });
+});
