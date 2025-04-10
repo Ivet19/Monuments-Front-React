dc4c1f3bc9f7660db1e9e729c818c98afde99a08
Ivet Lopez <ivet19@live.com>
2025-04-10 17:37:53 +0200

    Create MonumentCard component

---
+++ src/monument/components/monumentCard/monumentCard.tsx
@@ -0,0 +1,16 @@
+import Monument from "../../types";
+import "./monumentCard.css";
+
+interface MonumentCardProps {
+  monument: Monument;
+}
+
+const MonumentCard: React.FC<MonumentCardProps> = ({ monument: { name } }) => {
+  return (
+    <article className="monument">
+      <h2 className="monument__name">{name}</h2>
+    </article>
+  );
+};
+
+export default MonumentCard;
