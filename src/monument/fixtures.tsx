b6129ffc552e820fadc2d17709b92dd51a6cd69a
Ivet Lopez <ivet19@live.com>
2025-04-10 19:09:37 +0200

    Create fixtures

---
+++ src/monument/fixtures.tsx
@@ -0,0 +1,23 @@
+import Monument from "./types";
+
+export const simpsonsHouse: Monument = {
+  id: "47268910A",
+  name: "Simpson's house",
+  description:
+    "A familiar house with a big garden where one of the most famous families of the world lives in",
+  imageUrl: "http://simpsons.com/simpsonshouse.webp",
+  imageAlt: `general view of the ${name} monument`,
+  country: "USA",
+  city: "Springfield",
+};
+
+export const sagradaFamilia: Monument = {
+  id: "47268910B",
+  name: "Sagrada Familia",
+  description:
+    "A breathtaking basilica, blending Gothic and modernist architecture, still under construction.",
+  imageUrl: "http://sagradafamilia.com/sg.webp",
+  imageAlt: `general view of the ${name} monument`,
+  country: "Spain",
+  city: "Barcelona",
+};
