f66dbbd140708d13ddd0a1b39113c71494431573
Ivet Lopez <ivet19@live.com>
2025-04-13 19:44:13 +0200

    Create cerateMonument function in MonumentContextProvider

--- src/monument/context/types.ts
+++ src/monument/context/types.ts
@@ -1,8 +1,10 @@
 import { Monument } from "../types";
+import { MonumentData } from "../types";
 
 interface MonumentsContextStructure {
   monuments: Monument[];
   loadMonuments: () => Promise<void>;
+  createMonument: (monumentData: MonumentData) => Promise<void>;
 }
 
 export default MonumentsContextStructure;
