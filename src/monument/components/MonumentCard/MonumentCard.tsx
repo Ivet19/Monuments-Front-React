efc3ac08d39c9a7b4cdaf247ce4fabb77075e9c9
Ivet Lopez <ivet19@live.com>
2025-04-13 12:25:02 +0200

    Add image, description and location to MonumentCard

--- src/monument/components/MonumentCard/MonumentCard.tsx
+++ src/monument/components/MonumentCard/MonumentCard.tsx
@@ -5,10 +5,15 @@ interface MonumentCardProps {
   monument: Monument;
 }
 
-const MonumentCard: React.FC<MonumentCardProps> = ({ monument: { name } }) => {
+const MonumentCard: React.FC<MonumentCardProps> = ({
+  monument: { name, imageUrl, imageAlt, description, city, country },
+}) => {
   return (
     <article className="monument">
       <h2 className="monument__name">{name}</h2>
+      <img className="monument__image" src={imageUrl} alt={imageAlt} />
+      <p className="monument__description">{description}</p>
+      <span className="monument__location">{`${city}, ${country}`}</span>
     </article>
   );
 };
