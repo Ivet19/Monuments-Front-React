3043110775cfdc0f8b629b970e44555e54b98c2c
Ivet Lopez <ivet19@live.com>
2025-04-15 19:50:56 +0200

    Create delete button and delete monument

--- src/monument/components/MonumentCard/MonumentCard.tsx
+++ src/monument/components/MonumentCard/MonumentCard.tsx
@@ -1,4 +1,5 @@
 import { Monument } from "../../types";
+import useMonuments from "../../hooks/useMonuments";
 import "./MonumentCard.css";
 
 interface MonumentCardProps {
@@ -6,14 +7,22 @@ interface MonumentCardProps {
 }
 
 const MonumentCard: React.FC<MonumentCardProps> = ({
-  monument: { name, imageUrl, imageAlt, description, city, country },
+  monument: { id, name, imageUrl, imageAlt, description, city, country },
 }) => {
+  const { removeMonumentById } = useMonuments();
+
   return (
     <article className="monument">
       <h2 className="monument__name">{name}</h2>
       <img className="monument__image" src={imageUrl} alt={imageAlt} />
       <p className="monument__description">{description}</p>
       <span className="monument__location">{`${city}, ${country}`}</span>
+      <button
+        className="monument__delete-button"
+        onClick={() => removeMonumentById(id)}
+      >
+        Delete
+      </button>
     </article>
   );
 };
